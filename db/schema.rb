9dc2728485b83dd5679e5c2acd95e6f55172c81d
John Isaac Latigay <jclatigay@up.edu.ph>
2024-10-21 21:56:57 +0800

    User Email Create

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.2].define(version: 2024_10_21_082643) do
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
