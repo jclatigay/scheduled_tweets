9bac940b3ad92d63e93bce5e554c665cd45cae7a
John Isaac Latigay <jclatigay@up.edu.ph>
2024-10-23 02:25:52 +0800

    login with session cookies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,9 @@ class MainController < ApplicationController
   def index
     flash.now[:notice] = "Welcome to Scheduled Tweets"
     flash.now[:alert] = "Something went wrong"
+
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
   end
 end
