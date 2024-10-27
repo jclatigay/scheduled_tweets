41259ee1a0ef811c627e01621e84c0984dbe8023
John Isaac Latigay <jclatigay@up.edu.ph>
2024-10-27 16:39:22 +0800

    password reset update

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -12,4 +12,25 @@ class PasswordResetsController < ApplicationController
       redirect_to root_path, notice: "If an account exists with that email, we will send a link to reset your password."
     end
   end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "Your token has expired. Please try again."
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Your password was reset successfully. Please sign in"
+    else
+      render :edit, status: :bad_request
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
