e08e47d800518e5c13c3187291f4c37f85163d89
John Isaac Latigay <jclatigay@up.edu.ph>
2024-10-27 14:15:06 +0800

    reset password token mailer

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,7 +3,7 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-    @user.find_by(email: params[:email])
+    @user = User.find_by(email: params[:email])
 
     if @user.present?
       # send email
